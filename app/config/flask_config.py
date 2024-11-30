fd87693d216763708caade42bcccb31800ae02d2
Назим <96943330+ganyamede@users.noreply.github.com>
2024-11-30 23:57:18 +0200

    Update flask_config.py

--- app/config/flask_config.py
+++ app/config/flask_config.py
@@ -13,12 +13,12 @@ swagger = Swagger(app)
 CORS(app)
 
 #SQL
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://getapple:010203@localhost/getapple'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://...:...@localhost/...'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 #JWT
-app.config["JWT_SECRET_KEY"] = "64459822c292d8aee8da650590c9859cea0d1bfda97f1a31a928f787ae2666ae"
+app.config["JWT_SECRET_KEY"] = "..."
 app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)  # Срок действия access токена
 app.config["JWT_REFRESH_TOKEN_EXPIRES"] = timedelta(days=30)
 jwt = JWTManager(app)
