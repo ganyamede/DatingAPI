ba6ba2ef294ae89a92c22830d80abbfc20359df7
Назим <96943330+ganyamede@users.noreply.github.com>
2024-12-01 01:38:59 +0200

    Delete app directory

--- app/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from .likes import *
-from .client import *
-from .authorization import *
-from .other import *
--- app/security/user_encryption.py
+++
@@ -1,7 +0,0 @@
-import hashlib
-
-def hash_password(password):
-    return hashlib.sha256(password.encode('utf-8')).hexdigest()
-
-def verify_password(stored_hash, input_password):
-    return stored_hash == hash_password(input_password)
